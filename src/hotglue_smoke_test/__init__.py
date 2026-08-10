670cc3efa5044266cf9c343d974526e0c1fc3cc8
Otavio Trentin <otavio_trentin@hotmail.com>
2026-08-10 12:51:49 -0400

    bump version

--- src/hotglue_smoke_test/__init__.py
+++ src/hotglue_smoke_test/__init__.py
@@ -3,7 +3,7 @@
 from hotglue_smoke_test.etl import ETLSmokeRunner
 from hotglue_smoke_test.vcr import VCRTapTestRunner, VCRTargetTestRunner
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __all__ = [
     "ETLSmokeRunner",
